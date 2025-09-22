b7650bd414050a21baf5f062b5540e9d7ec2e210
Luke <lukembogo5@gmail.com>
2025-09-22 16:09:03 +0300

    resize

--- script.js
+++ script.js
@@ -16,3 +16,13 @@ document.querySelectorAll('.btn').forEach(function(button) {
         });
     }
 });
+
+burger = document.querySelector('burger');
+navbar = document.querySelector('navbar');
+navList = document.querySelector('nav-list');
+rightNav = document.querySelector('rightNav');
+burger.addEventListener('click', () => {
+    rightNav.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+});
